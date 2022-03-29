d46268b7fabcc6d4ffdf95bd7e29543ae2458e25
Riley Winkler <rex8112@gmail.com>
2022-03-29 00:36:10 -0500

    Create cache_manager.py

---
+++ innkeeper/caches/cache_manager.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from ..exceptions import NotFound
+from ..world import World
+
+class CacheManager:
+    def __init__(self, world: World) -> None:
+        self.world = world
+        self._cache = {}
+
+    # Private Methods
+
+    def _add_to_cache(self, key: str, value: object) -> None:
+        self._cache[key] = value
+
+    async def _fetch_entry(self, key: str) -> Optional[object]:
+        """Override this method to fetch from appropriate database table"""
+        pass
+
+    # Public Methods
+
+    def get(self, key: str) -> Optional[object]:
+        return self._cache.get(key, None)
+
+    async def fetch(self, key: str, force = True) -> object:
+        if key not in self._cache:
+            value = self._fetch_entry(key)
+            if value:
+                self._add_to_cache(key, value)
+            else:
+                raise NotFound(f"{key} not found")
+        else:
+            value = self.get(key)
+        return value
