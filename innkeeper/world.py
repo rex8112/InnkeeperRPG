31cc722eb08aa45d124b54dd242a5c396a097390
Riley Winkler <rex8112@gmail.com>
2022-03-29 00:35:58 -0500

    Create world.py

---
+++ innkeeper/world.py
@@ -0,0 +1,12 @@
+class World:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self._players = [] # TODO: Player Manager Class
+        self._characters = [] # TODO: Character Manager Class
+
+        self._continents = [] # TODO: Continent Manager Class
+        self._kingdoms = [] # TODO: Kingdom Manager Class
+        self._towns = [] # TODO: Town Manager Class
+        
+
+    
