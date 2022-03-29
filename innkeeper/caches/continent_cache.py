a98d02979fc42603316efcb31c692664b2a59dae
Riley Winkler <rex8112@gmail.com>
2022-03-29 00:36:12 -0500

    Create continent_cache.py

---
+++ innkeeper/caches/continent_cache.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from .cache_manager import CacheManager
+
+from ..world import World
+
+class ContinentCache(CacheManager):
+    def __init__(self, world: World) -> None:
+        super().__init__(world)
+
+    # Private Methods
+
+    async def _fetch_entry(self, key: str) -> Optional[object]:
+        pass # Implement database fetch here
