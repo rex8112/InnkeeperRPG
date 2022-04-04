0b4b9176d5fda52398b39610bbaab4af1ce92a7b
Riley Winkler <rex8112@gmail.com>
2022-04-04 04:54:50 -0500

    setup continent data structure
    Completes #1

--- innkeeper/continent.py
+++ innkeeper/continent.py
@@ -5,6 +5,30 @@ class Continent:
         self.world = world
         self.name = 'New Continent'
         self.size = 100
-        
-        self._towns = {}
-        self._kingdoms = {}
+        self.population = 0
+        self.towns = []
+        self.flags = []
+
+    # Serialization
+    def toJSON(self):
+        return {
+            'name': self.name,
+            'size': self.size,
+            'population': self.population,
+            'towns': self.towns,
+            'flags': self.flags,
+        }
+
+    # Deserialization
+    @classmethod
+    def fromJSON(cls, world: World, data: dict):
+        continent = cls(world)
+        continent.name = data['name']
+        continent.size = data['size']
+        continent.population = data['population']
+        continent.towns = data['towns']
+        continent.flags = data['flags']
+        return continent
+
+    def getKingdoms(self):
+        return self.world._kingdoms # TODO: Filter out the kingdoms that are not in this continent
--- innkeeper/world.py
+++ innkeeper/world.py
@@ -1,10 +1,14 @@
+from typing import List
+
+from .continent import Continent
+
 class World:
     def __init__(self, name: str) -> None:
         self.name = name
         self._players = [] # TODO: Player Manager Class
         self._characters = [] # TODO: Character Manager Class
 
-        self._continents = [] # TODO: Continent Manager Class
+        self._continents: List[Continent] = [] # TODO: Continent Manager Class
         self._kingdoms = [] # TODO: Kingdom Manager Class
         self._towns = [] # TODO: Town Manager Class
         
