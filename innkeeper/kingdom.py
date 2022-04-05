2cc16e80487bbea18f733bf4b311f77b1921e868
Riley Winkler <rex8112@gmail.com>
2022-04-05 03:37:18 -0500

    Add basic Kingdom class
    close #2

---
+++ innkeeper/kingdom.py
@@ -0,0 +1,37 @@
+from typing import Dict, List, Union
+from .world import World
+from .continent import Continent
+
+class Kingdom:
+    def __init__(self, world: World) -> None:
+        self.id = None
+        self.world = world
+        self.name = 'New Kingdom'
+        self.ruler = None
+        self.land = {}
+        self.population = 0
+        self.army = 0
+        self.flags = []
+
+    # Serialization
+    def toJSON(self) -> Dict[str, Union[str, int, List[str], None]]:
+        return {
+            'name': self.name,
+            'ruler': self.ruler.id if self.ruler else None,
+            'land': self.land,
+            'population': self.population,
+            'army': self.army,
+            'flags': self.flags,
+        }
+
+    # Deserialization
+    @classmethod
+    def fromJSON(cls, world: World, data: dict):
+        kingdom = cls(world)
+        kingdom.name = data['name']
+        kingdom.ruler = data['ruler']
+        kingdom.land = data['land']
+        kingdom.population = data['population']
+        kingdom.army = data['army']
+        kingdom.flags = data['flags']
+        return kingdom
