15bdef4a42595d63042d1607f83ea195ba77e5b9
Riley Winkler <rex8112@gmail.com>
2022-04-07 22:18:18 -0500

    Add basic EquipmentBlueprint class

---
+++ innkeeper/equipmentBlueprint.py
@@ -0,0 +1,44 @@
+from typing import Dict, List
+from .world import World
+from .attributeBlueprint import AttributeBlueprint
+
+class EquipmentBlueprint:
+    def __init__(self, world: World):
+        self.world = world
+        self.name = 'New Equipment Blueprint'
+        self.min_level = 1
+        self.max_level = 10
+        self.min_rarity = 0
+        self.max_rarity = 4
+        self.main_attributes: List[AttributeBlueprint] = []
+        self.rarity_attributes: List[AttributeBlueprint] = []
+
+    @classmethod
+    def from_dict(cls, world: World, data: dict):
+        blueprint = cls(world)
+        blueprint.name = data['name']
+        blueprint.min_level = data['min_level']
+        blueprint.max_level = data['max_level']
+        blueprint.min_rarity = data['min_rarity']
+        blueprint.max_rarity = data['max_rarity']
+
+        for d in data['main_attributes']:
+            attributeBlueprint = AttributeBlueprint.from_dict(d)
+            blueprint.main_attributes.append(attributeBlueprint)
+
+        for d in data['rarity_attributes']:
+            attributeBlueprint = AttributeBlueprint.from_dict(d)
+            blueprint.rarity_attributes.append(attributeBlueprint)
+
+        return blueprint
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'min_level': self.min_level,
+            'max_level': self.max_level,
+            'min_rarity': self.min_rarity,
+            'max_rarity': self.max_rarity,
+            'main_attributes': self.main_attributes,
+            'rarity_attributes': self.rarity_attributes,
+        }
