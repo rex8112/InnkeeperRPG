afcf3ac8341b75a0b19748218afadfc2df7be598
Riley Winkler <rex8112@gmail.com>
2022-04-08 21:06:43 -0500

    Add Snowflake class for future id generation

---
+++ innkeeper/abcs/snowflake.py
@@ -0,0 +1,42 @@
+import threading
+import datetime
+
+from ..world import World
+
+
+class Snowflake:
+    start_date = datetime.datetime(2022, 1, 1, tzinfo=datetime.timezone.utc)
+
+    def __init__(self, worker_id: int) -> None:
+        self.worker_id = worker_id
+        self.id_counter = 0
+        self.id_counter_lock = threading.Lock()
+        self.id_counter_lock.acquire()
+        self.id_counter_lock.release()
+
+    def get_new_id(self) -> int:
+        """Returns a new unique id.
+        
+        Binary format:
+        [63-22] Seconds since epoch+1640995200
+        [21-17] Worker id
+        [16-0] Id counter"""
+        self.id_counter_lock.acquire()
+        
+        count = self.id_counter
+        self.id_counter += 1
+        first = self.get_time_since_start() << 22
+        second = self.worker_id << 17
+        third = count
+
+        new_id = first | second | third
+
+        self.id_counter_lock.release()
+        return new_id
+
+    def get_datetime_from_id(self, id: int) -> datetime.datetime:
+        time_since_start = id >> 22
+        return self.start_date + datetime.timedelta(seconds=time_since_start)
+
+    def get_time_since_start(self) -> int:
+        return int((datetime.datetime.now(datetime.timezone.utc) - self.start_date).total_seconds())
