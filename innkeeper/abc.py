174e33216346d8b0526814e88145a8c2a3da1dd7
Riley Winkler <rex8112@gmail.com>
2022-04-09 12:33:09 -0500

    Add important methods to UniqueObject abc

--- innkeeper/abc.py
+++ innkeeper/abc.py
@@ -1,12 +1,28 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
+from .snowflake_generator import SnowflakeGenerator
 from .world import World
+
 
 class UniqueObject(ABC):
     @property
     @abstractmethod
     def id(self) -> int:
         pass
+
+    @abstractmethod
+    async def save(self) -> None:
+        pass
+
+    @abstractmethod
+    @classmethod
+    async def load(cls, world: World, id: int) -> 'UniqueObject':
+        pass
+
+    @property
+    def created_at(self) -> datetime:
+        return SnowflakeGenerator.get_datetime_from_id(self.id)
 
 class WorldObject(ABC):
     @property
