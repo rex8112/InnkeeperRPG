714eebeaaf48f1805bb6dc1c7a2be74aaa220371
Riley Winkler <rex8112@gmail.com>
2022-04-12 00:21:10 -0500

    Add PlayerCache

---
+++ innkeeper/caches/player_cache.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING, Optional
+
+from .cache_manager import CacheManager
+from ..player import Player
+
+if TYPE_CHECKING:
+    from ..world import World
+
+
+class PlayerCache(CacheManager):
+    def __init__(self, world: World) -> None:
+        super().__init__(world)
+
+    # Private Methods
+
+    async def _fetch_entry(self, key: str) -> Optional[object]:
+        player = Player.load(self.world, key)
+        return player
